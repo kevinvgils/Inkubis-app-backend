5fe29daf5747a3d905d3b1e02d882e7f969d7f71
ErenAygun6 <e.aygun2@student.avans.nl>
2023-01-16 14:29:40 +0100

    File limit changed to 100mb

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
   await app.listen(3000);
 }
 bootstrap();
